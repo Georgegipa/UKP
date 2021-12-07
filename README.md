0fc879ed6f5a8587229e400319ffed157081a0ae
Georgegipa_laptop <george.gipa@yahoo.gipa>
2021-12-07 03:45:29 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#UKP
+(Universal KeyPad)
+
+##Project Goals
+* Create a keypad with multiple profiles 
+* Buttons that trigger with interrupts
+* Load Profiles from micro_sd
+* Create a Profile template
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <Mouse.h>
+#include <Keyboard.h>
+
+#define button_1 15
+#define button_2 14
+#define button_3 16
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Mouse.begin(); 
+  Keyboard.begin();
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(button_1,INPUT);
+  pinMode(button_2,INPUT);
+  pinMode(button_3,INPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Button_1 state:");
+  Serial.println(digitalRead(button_1));
+  delay(250);
+  Serial.print("Button_2 state:");
+  Serial.println(digitalRead(button_2));
+  delay(250);
+  Serial.print("Button_3 state:");
+  Serial.println(digitalRead(button_3));
+  delay(250);  
 }
